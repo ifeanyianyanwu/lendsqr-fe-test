e4d7883f7e7a46f9fd1598da39d1090c44078f4b
IFEANYI ANYANWU <anyanwuifeanyi001@gmail.com>
2023-06-20 20:19:25 +0100

    developed dashboard page

--- src/pages/DashBoard/DashBoard.tsx
+++ src/pages/DashBoard/DashBoard.tsx
@@ -1,7 +1,29 @@
-import React from "react";
+import { useState } from "react";
+import Navbar from "../../components/Navbar/Navbar";
+import "./DashBoard.scss";
+import Sidebar from "../../components/Sidebar/Sidebar";
+import { Route, Routes } from "react-router-dom";
+import Users from "../../components/Users/Users";
+import ToggleSidebar from "../../components/ToggleSidebar/ToggleSidebar";
 
 const DashBoard = () => {
-  return <div>DashBoard</div>;
+  const [toggleSidebar, setToggleSidebar] = useState<boolean>(false);
+
+  return (
+    <div className="dashboard__page-container">
+      <Navbar setToggleSidebar={setToggleSidebar} />
+      <div className="dashboard__secondary-container">
+        <Sidebar />
+        {toggleSidebar && <ToggleSidebar setToggleSidebar={setToggleSidebar} />}
+        <div className="dashboard__link-content-container">
+          <Routes>
+            <Route path="/users" element={<Users />} />
+            <Route path="/user_details" element={"user details"} />
+          </Routes>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default DashBoard;
