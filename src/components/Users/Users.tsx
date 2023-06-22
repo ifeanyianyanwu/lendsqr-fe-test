05a921d843d4eadbb271a855bfe3c25f9ae4f313
IFEANYI ANYANWU <anyanwuifeanyi001@gmail.com>
2023-06-22 23:13:48 +0100

    created users page

---
+++ src/components/Users/Users.tsx
@@ -0,0 +1,48 @@
+import "./Users.scss";
+import Table from "../Table/Table";
+import usersData from "../../helpers/users.json";
+
+import { USERS_CARD_DATA } from "../../helpers/utility";
+import Card from "../../layout/Card/Card";
+import TabPage from "../../layout/TabPage/TabPage";
+
+const Users = () => {
+  const columns = [
+    { value: "ORGANIZATION" },
+    { value: "USERNAME" },
+    { value: "EMAIL" },
+    { value: "PHONE NUMBER" },
+    { value: "DATE JOINED" },
+    { value: "STATUS" },
+    { value: " " },
+  ];
+
+  const data = usersData?.map((item) => ({
+    id: item._id,
+    organization: item.organization,
+    username: item.username,
+    email: item.personalInformation.email,
+    phoneNumber: item.personalInformation.phoneNumber,
+    dateJoined: item.dateJoined.split("T")[0],
+    status: item.status,
+    options: "",
+  }));
+
+  return (
+    <TabPage title="users">
+      <div className="users__cards-container">
+        {USERS_CARD_DATA.map((data, index) => (
+          <Card
+            key={index}
+            title={data.title}
+            img_url={data.img_url}
+            value={data.value}
+          />
+        ))}
+      </div>
+      <Table columns={columns} initialData={data} itemsPerPage={9} />
+    </TabPage>
+  );
+};
+
+export default Users;
