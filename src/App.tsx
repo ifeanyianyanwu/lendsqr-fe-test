d88f3e130722490316e9f424633218b8cc2a3fc0
IFEANYI ANYANWU <anyanwuifeanyi001@gmail.com>
2023-06-22 01:48:27 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.scss";
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { DashBoard, Login } from "./pages";
 function App() {
   return (
